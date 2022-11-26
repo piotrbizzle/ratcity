f16a16fd20f9cb8113bf74e544223718af91f9d3
piotrbizzle <100106309+piotrbizzle@users.noreply.github.com>
2022-11-26 10:18:44 -0500

    not all walls are platforms

---
+++ Wall.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+	// collision
+        this.gameObject.AddComponent<BoxCollider2D>();
+
+	Rigidbody2D rb = this.gameObject.AddComponent<Rigidbody2D>();
+	rb.gravityScale = 0.0f;
+	rb.constraints = RigidbodyConstraints2D.FreezeAll;
+	rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+	rb.sleepMode = RigidbodySleepMode2D.NeverSleep;       
+    }
+}
